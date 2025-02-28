b7b7c77f83288409f7a0d339b2284a8517aaeb21
Essayas85 <essayas.kebe@outlook.fr>
2025-02-28 12:50:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function changerTexte() {
-   document.getElementById("message").innerText = "Bravo ! Vous avez cliqué sur le bouton ! 🎉";
+    document.getElementById("message").innerText = "Bravo ! Vous avez cliqué sur le bouton ! 🎉";
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }
